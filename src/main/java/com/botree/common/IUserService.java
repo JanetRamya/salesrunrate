adf18c13d6515e1526d68f9c5e44b48195ea20b0
JanetRamya <janetramya94@gmail.com>
2017-02-14 18:02:50 +0530

    Alteration in Topbar

--- src/main/java/com/botree/common/IUserService.java
+++ src/main/java/com/botree/common/IUserService.java
@@ -3,7 +3,7 @@ package com.botree.common;
 import com.botree.salesrunrate.entity.UserProfile;
 
 public interface IUserService {
-	UserProfile findUser(String userName,String password);
+	UserProfile findUser(String userName, String password);
 
 	UserProfile findOneByUserName(String userName);
 
--- src/main/java/com/botree/salesrunrate/entity/DistributorTarget.java
+++ src/main/java/com/botree/salesrunrate/entity/DistributorTarget.java
@@ -2,13 +2,12 @@ package com.botree.salesrunrate.entity;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="DistributorTarget")
-public class DistributorTarget extends MongoEntity{
+@Document(collection = "DistributorTarget")
+public class DistributorTarget extends MongoEntity {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
 
 }
